9ede9ff0d8219ec666c5e363b541b6eaf4123135
May <yanjiemay69@gmail.com>
2021-08-21 12:54:46 -0400

    sdfds

--- src/main/java/cucumberTest/TestRunner.java
+++ src/main/java/cucumberTest/TestRunner.java
@@ -1,14 +1,14 @@
 package cucumberTest;
 
 import org.junit.runner.RunWith;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Feature/LoginTest.feature",
+@CucumberOptions(features = "Feature/TestCucumberbackground.feature",
 glue = "stepDefinition", monochrome = true,
 plugin = {"pretty", "html:reports/cucumber-report.html" })
 public class TestRunner {
-
 }
+
