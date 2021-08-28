3f922dc5acc7e910aa0642a766a4b5541564734b
Sophiashe88 <75906288+Sophiashe88@users.noreply.github.com>
2021-08-28 16:59:16 -0400

    ssss

--- src/main/java/stepDefinition/WebDriverManager.java
+++ src/main/java/stepDefinition/WebDriverManager.java
@@ -1,17 +1,27 @@
 package stepDefinition;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.bora.enums.EnvironmentType;
 
 public class WebDriverManager {
 
 	private WebDriver driver;
+
+	private String browserType;
+	private EnvironmentType environmentType;
+
+	public void createddriver() {
+		if(environmentType == EnvironmentType.LOCAL){
+		driver =new ChromeDriver();
+		
+	}else if(environmentType==EnvironmentType.REMOTE) {
+
 	
-	
-	
-	
-	
-	
-	
-	
-	
+		driver = new RemoteWebDriver(null);
+	}
+
+}
 }
