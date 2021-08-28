d51f4e67a99af7822d78469fbda7914a3ade0ea0
Nuerzhati Yijiati <nurzatejat@live.com>
2021-08-28 16:18:16 -0400

    Nur

--- src/test/java/inClass/Java/task/InClass_Combination.java
+++ src/test/java/inClass/Java/task/InClass_Combination.java
@@ -1,16 +1,49 @@
 package inClass.Java.task;
 
+import java.util.Arrays;
+
 public class InClass_Combination {
 	
 	/** 
 		1.Given an integer, return true if it is within 13 of 237 or 345. 
 			Note: Math.abs(num) computes the absolute value of a number.
 			
+			num -->  num-13 - num+13
+			
+			i  --> 224 - 250    --> (i>=224 && i<=250)
+			i  --> 332 - 358    --> (i>=332 && i<=358)
+			-347 --> Math.abs() --> 347
+			
+			return (i>=224 && i<=250) ||  (i>=332 && i<=358)
+			
 		2.We'll say a number is special if it is a multiple of 13 OR if it is one more/less than a multiple of 13. 
 			Return true if the given non-negative number is special.
 		
+		13% 13 =0 --> special -> true
+		
+		27%13 = 1 --> special -> true
+		
+		25%13 = 12 --> special -> true
+		
+		
+		
+		
 		3.Given 2 integer, a and b, return their sum. 
 			However, if sums in the range between 8 - 17, then just return 20.
+		
+		int sum = a+b;
+		
+		if ( sum<=17 && sum >=8) return 20;
+		return sum;
+		
+		return (sum<=17 && sum >=8) ? 20:sum;
+		--> if(sum<=17 && sum >=8) {
+		    	20
+		    }else{
+		    	sum
+		    }
+		
+		
 		
 		4.Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
 
@@ -18,8 +51,37 @@ public class InClass_Combination {
 			input([5, 11, 9]) expecting output [11, 9, 5]
 			input([7, 0, 0])  expecting output [0, 0, 7]
 			
+			int[] result = {  array[1], array[2], array[0]  };
+			
+			{12,24,34,54,23,43,16,46,24,83}
+			
+			{24,34,54,23,43,16,46,24,83,12}
+			int[] result = new int[  array.length ];
+			
+			for( int i=0; i< array.length-2; i++){
+				result[ i ]= array[ i+1 ] 
+			}
+			result [ array.length-1] = 	 array[0];
+			
+			
+			
 		5.optional
 			https://codingbat.com/prob/p199612
 			https://codingbat.com/prob/p138990
 	 */
+	
+	
+	public static void main(String[] args) {
+		
+		int[] array = {12,24,34};
+		
+		int[] result = new int[  array.length ];
+		
+		for( int i=0; i<= array.length-2; i++){
+			result[ i ]= array[ i+1 ]; 
+		}
+		result [ array.length-1] = 	 array[0];
+		
+		System.out.println(Arrays.toString(result));
+	}
 }
--- src/test/java/inClass/Java/task/InClass_String.java
+++ src/test/java/inClass/Java/task/InClass_String.java
@@ -12,6 +12,11 @@ public class InClass_String {
 			input("java") → "vaja"
 			input("Hi") → "Hi"
 	 
+	 if str.length <3 / <=2  --> return str
+	 str.subString(0,2) --> first part
+	 str.sbuString(2,str.length()) --> second part
+	 
+	 
 	 
 	 2.Given a string of odd length, return a string made of the middle three chars, so the string "strings" yields "rin". 
 	 	The string length will be at least 3.
@@ -19,16 +24,45 @@ public class InClass_String {
 			input("strings") → "rin"
 			input("codes") → "ode"
 			input("Practices") → "cti"
+			
+			int startIndex = str.length/2 - 1  --> 2
+			int endIndex = str.length/2 + 1    --> 4
+			str.subString( 2 , 4+1)   
+			
+
+			
 	 
 	 3.Given a string, return true if it ends in "es".
 
 			input("codes") → true
 			input("s") → false
 			input("Practice") → false
+			
+			subString() -> 
+			str.subString(    str.length-2     )
+			
+			
+			
 	 
 	 4.optional
 	 	https://codingbat.com/prob/p122943
 	 	
 	 */
+	
+	
+	
+	public static void main(String[] args) {
+		String test = "kjashfkaj0";
+		String code = "s";
+		code.substring(code.length()-2);
+		System.out.println();
+		
+		
+		
+		
+		
+		
+//		test.subst
+	}
 
 }
